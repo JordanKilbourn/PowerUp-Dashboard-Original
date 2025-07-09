e7629f499e2f1cfd329075292520dfcb7833d8d3
Jordan K. <jordan.kilbourn@playpower.com>
2025-07-09 09:20:53 -0400

    Create include.js

---
+++ scripts/include.js
@@ -0,0 +1,12 @@
+// /scripts/include.js
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll('[data-include]').forEach(async el => {
+    const file = el.getAttribute('data-include');
+    const res = await fetch(file);
+    if (res.ok) {
+      el.innerHTML = await res.text();
+    } else {
+      el.innerHTML = `<p>Failed to load ${file}</p>`;
+    }
+  });
+});
