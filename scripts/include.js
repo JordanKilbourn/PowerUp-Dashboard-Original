f6ccfc8fbc0b4f2a0a19c816068cfc5add81bc7b
Jordan K. <jordan.kilbourn@playpower.com>
2025-07-09 10:29:26 -0400

    Update include.js

--- scripts/include.js
+++ scripts/include.js
@@ -1,8 +1,12 @@
 // /scripts/include.js
-document.addEventListener("DOMContentLoaded", () => {
-  const includes = document.querySelectorAll('[id]');
+function includeHTML(callback) {
+  const includes = document.querySelectorAll('[w3-include-html]');
+  let remaining = includes.length;
+
+  if (remaining === 0 && typeof callback === "function") callback();
+
   includes.forEach(async (el) => {
-    const file = `/components/${el.id}.html`;
+    const file = el.getAttribute("w3-include-html");
     try {
       const res = await fetch(file);
       if (!res.ok) throw new Error(`Missing include: ${file}`);
@@ -10,6 +14,10 @@ document.addEventListener("DOMContentLoaded", () => {
     } catch (e) {
       console.error(e);
     }
+
+    remaining--;
+    if (remaining === 0 && typeof callback === "function") callback();
   });
-});
+}
+
 
