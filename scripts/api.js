509b9306711da5643051d5d3f8bfaad419bc476d
Jordan K. <jordan.kilbourn@playpower.com>
2025-07-14 11:27:59 -0400

    Update api.js

--- scripts/api.js
+++ scripts/api.js
@@ -1,1 +1,22 @@
+// /scripts/api.js
+
+const SHEET_IDS = {
+  powerHours: '1240392906264452',
+  levelTracker: '8346763116105604',
+  ciSubmissions: '7397205473185668',
+  safetyConcerns: '4089265651666820',
+  qualityCatches: '1431258165890948'
+};
+
+const API_PROXY = 'https://powerup-proxy.onrender.com';
+
+function fetchSheet(id) {
+  return fetch(`${API_PROXY}/sheet/${id}`).then(res => res.json());
+}
+
+function fetchReport(id) {
+  return fetch(`${API_PROXY}/report/${id}`).then(res => res.json());
+}
+
+export { SHEET_IDS, fetchSheet, fetchReport };
 
